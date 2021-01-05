c225861a3c0ceae07858e3a9f412372c8a7ddb2a
Nathan Lehman <nlehman@dlcouch.com>
2021-01-05 09:25:59 -0500

    WIP

---
+++ src/main/java/guru/springframework/Expression.java
@@ -0,0 +1,7 @@
+package guru.springframework;
+
+public interface Expression {
+    Money reduce(Bank bank, String to);
+    public Expression plus(Expression addend);
+    public Expression times(int multiplier);
+}
